c6ec40301b73eda74b36b00adad37a45d50c5b25
Tony <tonykurosaki117@gmail.com>
2023-04-13 21:39:04 -0600

    scrollbar on chat added

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -2,7 +2,7 @@ import { Button, Icon, IconButton } from "@mui/material"
 import { EmojiEmotions, Send, Image  } from '@mui/icons-material'
 import Picker from '@emoji-mart/react'
 import { Message } from "./Message"
-import { ChangeEvent, useEffect, useState } from "react"
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
 import { useAuth } from "@/context/AuthContext"
 import type{ SendMessageType } from "@/types/Provider"
 import type { MessagesType } from "@/types/Messages"
@@ -40,11 +40,21 @@ export const ChatContainer = ({id} : ChatContainerProps ) => {
     }
   }
 
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    handleSendMessage();
+  }
+
   console.log(user?.photoURL)
   return(
     <>
       <div className="w-full h-full bg-cyan-950 bg-opacity-70 rounded-r-xl">
-        <div className="w-full h-9/10 p-5">
+        <div className="w-full h-5/100 bg-cyan-600 bg-opacity-60 rounded-tr-xl flex items-center px-4">
+          <p>
+            Chat test
+          </p>
+        </div>
+        <div className="w-full h-17/20 p-2 overflow-y-scroll">
           <div className="flex flex-col gap-2">
             {
               messages?.map((message: MessagesType, i: string) => 
@@ -54,7 +64,9 @@ export const ChatContainer = ({id} : ChatContainerProps ) => {
           </div>
         </div>
         <div className="w-full bg-cyan-950 bg-opacity-90 h-1/10 rounded-br-xl flex p-4">
-          <input type="text" onChange={handleWriteMessage} onSubmit={handleSendMessage} value={messageValue} className="bg-cyan-600 bg-opacity-60 border-none rounded w-9/10 px-1 w-p:w-4/5"/>
+          <form onSubmit={handleSubmit} className="w-9/10 w-p:w-4/5 flex">
+            <input type="text" onChange={handleWriteMessage} value={messageValue} className="bg-cyan-600 bg-opacity-60 border-none rounded px-1 w-full"/>
+          </form>
           <div className="w-1/10 grid grid-cols-3 items-center px-2 gap-2 w-p:w-1/5">
             <Icon component={EmojiEmotions} fontSize={"medium"} className=" hover:text-cyan-400 cursor-pointer"/>
            { /*<IconButton color="primary" aria-label="upload picture" component="label">
