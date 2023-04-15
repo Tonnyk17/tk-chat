cf356d820ea530172fa5f7e93aa2d0fbed3f5a16
Tony <tonykurosaki117@gmail.com>
2023-04-15 04:03:20 -0600

    Add participants function fixed

--- src/components/CreateChatModal.tsx
+++ src/components/CreateChatModal.tsx
@@ -4,7 +4,7 @@ import { Icon } from "@mui/material"
 import { ChangeEvent, FormEvent, useState } from "react";
 
 export const CreateChatModal = () => {
-  const { closeModal, isClosedModal, createRoom } = useAuth();
+  const { closeModal, isClosedModal, createRoom, user } = useAuth();
   const [chatName, setChatName] = useState<string>();
   const [chatParticipants, setChatParticipants] = useState<string[]>();
   const [participantsString, setParticipantsString] = useState<string>('');
@@ -26,7 +26,7 @@ export const CreateChatModal = () => {
 
   const handleCreateRoom = () => {
     if(chatName && chatParticipants){
-      createRoom(chatName, chatParticipants);
+      createRoom(chatName, [...chatParticipants, String(user?.email)]);
       closeModal();
       setChatName('');
       setChatParticipants([]);
@@ -49,7 +49,7 @@ export const CreateChatModal = () => {
      {
         !isClosedModal &&  
         <div className="w-screen h-screen absolute left-0 top-0 bg-gray-800  bg-opacity-80 flex justify-center items-center">
-          <div className="w-p:w-9/10 w-p:h-9/10 w-t:w-9/10 w-t:h-1/2 w-d:w-1/2 w-d:h-1/2 bg-cyan-600 bg-opacity-70 rounded-lg border-2 border-cyan-300 flex flex-col items-end shadow-md shadow-cyan-300 p-5 gap-5">
+          <div className="w-p:w-9/10 w-p:h-9/10 w-t:w-9/10 w-t:h-1/2 w-d:w-1/2 w-d:h-1/2 bg-cyan-600 bg-opacity-70 rounded-lg border-2 border-cyan-300 flex flex-col items-end shadow-md shadow-cyan-300 p-5 gap-3">
             <Icon 
               component={Close} 
               fontSize={"medium"} 
@@ -72,6 +72,9 @@ export const CreateChatModal = () => {
                     Make sure that each email address is a Gmail address. (For example: email@gmail.com,test@gmail.com)
                   </li>               
                 </ol>
+                <p>
+                  Note: Do not include your email in the list, it will be added automatically.
+                </p>
               </div>
               <form onSubmit={handleSubmit} className="w-full flex flex-col justify-center items-center gap-7">
                 <div className="flex flex-col w-p:w-9/10 w-1/2">
