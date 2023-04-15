f09718ebc41fae8bb18a42f891308e1ec42aff96
Tony <tonykurosaki117@gmail.com>
2023-04-14 20:37:36 -0600

    addChat component added

---
+++ src/components/AddChat.tsx
@@ -0,0 +1,17 @@
+import { Add } from "@mui/icons-material"
+import { Icon } from "@mui/material"
+
+export const AddChat = () => {
+
+  return(
+    <>
+      <div 
+        className="w-p:rounded-none w-t:rounded-none w-full h-full bg-cyan-950 bg-opacity-70 rounded-r-xl flex flex-col justify-center items-center gap-4 hover:bg-cyan-800 hover:opacity-80 cursor-pointer"
+        onClick={() => console.log("Add")}  
+      >
+        <Icon component={Add} fontSize={"large"} className=" bg-cyan-600 rounded-full bg-opacity-95"/>
+        <p className=" text-cyan-300">Click to create a chat room</p>
+      </div>   
+    </>
+  )
+}
