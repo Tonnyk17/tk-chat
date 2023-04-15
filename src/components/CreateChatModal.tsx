047f189905f6492297c43e81eda04386aaec5e37
Tony <tonykurosaki117@gmail.com>
2023-04-15 04:07:17 -0600

    V1 finished

--- src/components/CreateChatModal.tsx
+++ src/components/CreateChatModal.tsx
@@ -6,8 +6,8 @@ import { ChangeEvent, FormEvent, useState } from "react";
 export const CreateChatModal = () => {
   const { closeModal, isClosedModal, createRoom, user } = useAuth();
   const [chatName, setChatName] = useState<string>();
-  const [chatParticipants, setChatParticipants] = useState<string[]>();
-  const [participantsString, setParticipantsString] = useState<string>('');
+  const [chatMembers, setChatMembers] = useState<string[]>();
+  const [membersString, setMembersString] = useState<string>('');
 
   const handleWriteName = (e:ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
@@ -15,28 +15,28 @@ export const CreateChatModal = () => {
     setChatName(inputValue)
   }
 
-  const handleWriteParticipants = (e:ChangeEvent<HTMLInputElement>) => {
+  const handleWriteMembers = (e:ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
     const inputValue = (e.target as HTMLInputElement).value;
-    const listParticipants = inputValue.split(",");
-    const participants = listParticipants.map((item) => item.trim());
-    setParticipantsString(inputValue)    
-    setChatParticipants(participants)
+    const listMembers = inputValue.split(",");
+    const members = listMembers.map((item) => item.trim());
+    setMembersString(inputValue)    
+    setChatMembers(members)
   }
 
   const handleCreateRoom = () => {
-    if(chatName && chatParticipants){
-      createRoom(chatName, [...chatParticipants, String(user?.email)]);
+    if(chatName && chatMembers){
+      createRoom(chatName, [...chatMembers, String(user?.email)]);
       closeModal();
       setChatName('');
-      setChatParticipants([]);
+      setChatMembers([]);
     }
   }
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const partRegex = /^[\w-\.]+@gmail\.com(?:,[\w-\.]+@gmail\.com){0,19}$/;
-    const isValidate = partRegex.test(participantsString)
+    const isValidate = partRegex.test(membersString)
     if(isValidate){
       handleCreateRoom();
     } else {
@@ -59,14 +59,14 @@ export const CreateChatModal = () => {
             <div className="w-full h-full flex flex-col gap-5 items-center">
               <div className="w-9/10 flex flex-col items-start gap-2 w-p:text-sm w-t:text-base text-cyan-300 font-semibold">
                 <p>
-                  To add participants to the chat, follow these steps:
+                  To add members to the chat, follow these steps:
                 </p>
                 <ol className="list-decimal">
                   <li>
                     Write a name of the chat room
                   </li>
                   <li>
-                    Write the email address of each participant separated by a comma.
+                    Write the email address of each member separated by a comma.
                   </li>
                   <li>
                     Make sure that each email address is a Gmail address. (For example: email@gmail.com,test@gmail.com)
@@ -90,15 +90,15 @@ export const CreateChatModal = () => {
                   />
                 </div>
                 <div className="flex flex-col w-p:w-9/10 w-1/2">
-                  <label htmlFor="participants" className="text-cyan-200">Participants:</label>
+                  <label htmlFor="members" className="text-cyan-200">Members:</label>
                   <input 
                     type="text" 
                     required
                     placeholder="email@gmail.com" 
-                    id="participants" 
-                    value={chatParticipants}
+                    id="members" 
+                    value={chatMembers}
                     className="w-full h-8 bg-cyan-950 bg-opacity-90 border-none rounded px-1 focus:outline"
-                    onChange={handleWriteParticipants}
+                    onChange={handleWriteMembers}
                   />
                 </div>
                 <button className=' bg-purple-900 p-2 rounded-md hover:opacity-70 flex gap-2'>
